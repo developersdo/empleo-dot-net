aea5ff544e73e9964b4504dc21938d6dd1e0fa58
Enmanuel Toribio <eats007@gmail.com>
2014-05-27 23:45:34 -0400

    Fixed Ahmed observations

--- EmpleoDotNet/Models/Entities/JobOffer.cs
+++ EmpleoDotNet/Models/Entities/JobOffer.cs
@@ -10,6 +10,12 @@ namespace EmpleoDotNet.Models.Entities
         public Int32 UserId { get; set; }
         public String Title { get; set; }
         public String Description { get; set; }
+
+
+        public String ContactName { get; set; }
+        public String ContactPhone { get; set; }
+        public String ContactEmail { get; set; }
+        public String ContactWebSite { get; set; }
         
         public Boolean IsPublished { get; set; }
         public DateTime PublishedDate { get; set; }
