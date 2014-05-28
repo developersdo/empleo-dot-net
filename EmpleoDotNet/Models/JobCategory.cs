7d69a43dad26c096d8199356bb20a5c78d0ad53b
castilloss_lead <lyrc@castilloss.com>
2014-05-28 08:56:01 -0400

    Definicion de entidades JobOpportunity y JobCategory(enum)

---
+++ EmpleoDotNet/Models/JobCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace EmpleoDotNet.Models
+{
+    public enum JobCategory
+    {
+         None = 0,
+         GraphicDesign = 1,
+         WebDevelopment = 2,
+         MobileDevelopment = 3,
+         SoftwareDevelopment = 4,
+         SystemAdministrator = 5,
+         Networking = 6,
+         ItSales = 7
+    }
+}
---
+++ EmpleoDotNet/Models/JobOpportunity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmpleoDotNet.Models
+{
+    public class JobOpportunity
+    {
+        public Int32 JobOpportunityId { get; set; }
+        public String Place { get; set; }
+        public JobCategory Category { get; set; }
+        public String Profile { get; set; }
+        public String RequirementsToApply { get; set; }
+        public String CompanyName { get; set; }
+        public String CompanyUrl { get; set; }
+        public String CompanyEmail { get; set; }
+        public Byte?[] CompanyLogo { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
