7d1e1254c1c6a634037c564bda5cd96698f7dba6
Enmanuel Toribio <eats007@gmail.com>
2014-05-27 23:45:34 -0400

    Fixed Ahmed observations

--- EmpleoDotNet/Models/Entities/Entity.cs
+++ EmpleoDotNet/Models/Entities/Entity.cs
@@ -5,6 +5,10 @@ using System.Web;
 
 namespace EmpleoDotNet.Models.Entities
 {
+    /// <summary>
+    /// Las entidades deben poseer un identificador unico para poder buscar entidades especificas cuando trabajemos con colecciones.
+    /// Las entidades deben poseer un campo con la fecha de creación para posibles auditorias o como referencia en potenciales sesiones de debugging.
+    /// </summary>
     public class Entity
     {
         public Int32 Id { get; set; }
