fe9b0f5ad0a04f7ee559991f2da61e5a5e5f2aff
amhed <aherrera@gmail.com>
2014-05-28 10:30:13 -0400

    Limpiand un poco el codigo
    * El commit previo tenia algunos errores de referencias, fueron arreglados
    * Organice un poco el folder Models en folders
    * Renombre la clase Entity a EntityBase y la hice abstracta
    * Agregue un ejemplo de descripcion en JobCategoryEnum

---
+++ EmpleoDotNet/Models/EntityBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmpleoDotNet.Models
+{
+    /// <summary>
+    /// Clase base de donde heredan todas las entidades del sistema. 
+    /// Evita repetición de código y ayuda a mantener los modelos limpios
+    /// </summary>
+    public abstract class EntityBase
+    {
+
+        /// <summary>
+        /// Las entidades deben poseer un identificador unico para poder realizar búsquedas
+        /// cuando trabajemos con colecciones.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Las entidades deben poseer un campo con la fecha de creación para posibles 
+        /// auditorias o como referencia en potenciales sesiones de debugging.
+        /// </summary>
+        public DateTime Created { get; set; }
+    }
+}
--- EmpleoDotNet/Models/JobCategory.cs
+++ EmpleoDotNet/Models/JobCategory.cs
@@ -1,14 +1,18 @@
-﻿namespace EmpleoDotNet.Models
+﻿using System.ComponentModel;
+
+namespace EmpleoDotNet.Models
 {
     public enum JobCategory
     {
-         None = 0,
-         GraphicDesign = 1,
-         WebDevelopment = 2,
-         MobileDevelopment = 3,
-         SoftwareDevelopment = 4,
-         SystemAdministrator = 5,
-         Networking = 6,
-         ItSales = 7
+        [Description("Ninguna")]
+        None = 0,
+
+        GraphicDesign = 1,
+        WebDevelopment = 2,
+        MobileDevelopment = 3,
+        SoftwareDevelopment = 4,
+        SystemAdministrator = 5,
+        Networking = 6,
+        ItSales = 7
     }
 }
