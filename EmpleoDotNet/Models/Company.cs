e1717436a4f6f88dbf0d4edb4a3f821391fd7c23
sabdielrivera <sabdiel.rivera@gmail.com>
2014-05-27 22:36:10 -0700

    Definición de entidad principal

---
+++ EmpleoDotNet/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet.Models
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Email { get; set; }
+        public byte[] Logo { get; set; }
+    }
+}
