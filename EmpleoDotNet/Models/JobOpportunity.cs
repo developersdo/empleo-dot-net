4768f478dc8aca4140e5d2512c0488a8516d74ed
amhed <aherrera@gmail.com>
2014-05-30 19:19:34 -0400

    Se hizo un refactoring pequeño de la clase JobOpportunity

--- EmpleoDotNet/Models/JobOpportunity.cs
+++ EmpleoDotNet/Models/JobOpportunity.cs
@@ -12,16 +12,20 @@ namespace EmpleoDotNet.Models
         /// <summary>
         /// Posición geográfica (donde es el trabajo)
         /// </summary>
-        public String Location { get; set; }
+        public string Location { get; set; }
+        
         public JobCategory Category { get; set; }
         
-        //TODO: Que es Profile?
-        public String Profile { get; set; }
-        public String RequirementsToApply { get; set; }
-        public String CompanyName { get; set; }
-        public String CompanyUrl { get; set; }
-        public String CompanyEmail { get; set; }
-        public Byte?[] CompanyLogo { get; set; }
+        public string RequirementsToApply { get; set; }
+        
+        public string CompanyName { get; set; }
+        
+        public string CompanyUrl { get; set; }
+        
+        public string CompanyEmail { get; set; }
+        
+        public string CompanyLogoUrl { get; set; }
+        
         public DateTime PublishedDate { get; set; }
     }
 }
