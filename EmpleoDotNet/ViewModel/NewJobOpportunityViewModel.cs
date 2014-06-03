49b2bac6346891e00c37b3dd7e316b675558bd4d
ManuDev01 <HernandezManuel370@gmail.com>
2014-06-02 23:51:24 -0430

    Agregando restricciones al ViewModel de agregar vacantes

--- EmpleoDotNet/ViewModel/NewJobOpportunityViewModel.cs
+++ EmpleoDotNet/ViewModel/NewJobOpportunityViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Permissions;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 using EmpleoDotNet.Models;
 
 namespace EmpleoDotNet.ViewModel
@@ -12,19 +8,27 @@ namespace EmpleoDotNet.ViewModel
     /// </summary>
     public class NewJobOpportunityViewModel
     {
+        [Required(ErrorMessage = "El campo titulo es requerido."), Display(Name = "Titulo")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "El campo ubicación es requerido."), Display(Name = "Ubicación")]
         public string Location { get; set; }
 
+        [Required(ErrorMessage = "Debe elegir una categoria."), Display(Name = "Categoria")]
         public JobCategory Category { get; set; }
 
+        [Required(ErrorMessage = "Debe especificar al menos un requisito."), Display(Name = "Requisitos para aplicar")]
         public string RequirementsToApply { get; set; }
-   
+
+        [Required(ErrorMessage = "El nombre de la empresa es requerido."), Display(Name = "Ubicación")]
         public string CompanyName { get; set; }
 
+        [Display(Name = "Dirección Web")]
         public string CompanyUrl { get; set; }
 
+        [Required(ErrorMessage = "Debe especificar al menos una dirección de correo electrónica."), Display(Name = "Email")]
         public string CompanyEmail { get; set; }
+
 
         public string CompanyLogoUrl { get; set; }
 
