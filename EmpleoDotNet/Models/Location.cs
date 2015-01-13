44860a1c901c48b6cfde45a80e11a3a73c3a0f20
unknown <=>
2015-01-13 17:35:59 -0400

    Crea entidad Location

---
+++ EmpleoDotNet/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet.Models
+{
+    public class Location:ISearchable
+    {
+        /// <summary>
+        /// Identificador único de cada ciudad
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
