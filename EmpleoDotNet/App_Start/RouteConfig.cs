bd03e1047e31d9e417065d1586053d7db4a24b75
amhed <aherrera@gmail.com>
2015-01-31 17:40:27 -0800

    Pretty URL:
    - Agregando nueva ruta en RouteConfig.
    - Agregando nuevo ejemplo de como llamar dicha ruta en Index.cshtml
    - Agregando paquete RouteDebugger para visualizar las rutas creadas

--- EmpleoDotNet/App_Start/RouteConfig.cs
+++ EmpleoDotNet/App_Start/RouteConfig.cs
@@ -13,11 +13,23 @@ namespace EmpleoDotNet
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+                name: "JobOpportunity-Pretty",
+                url: "JobOpportunity/Detail/{id}/{description}",
+                defaults: new {
+                    controller = "JobOpportunity", action = "Detail", id = UrlParameter.Optional, description = UrlParameter.Optional
+                }
+            );
+
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
---
+++ EmpleoDotNet/Helpers/PrettyUrlHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet.Helpers
+{
+    public class PrettyUrlHelper
+    {
+        public static string Beautify(string str)
+        {
+            //TODO: Este método es solo una demostración. Hay que implementarlo mejor
+            return str.ToLower()
+                .Replace(".","")
+                .Replace(",", "")
+                .Replace(" ", "-")
+                .Replace("ñ", "n")
+                .Replace("á", "a")
+                .Replace("é", "e")
+                .Replace("í", "i")
+                .Replace("ó", "o")
+                .Replace("ú", "u");
+        }
+    }
+}
