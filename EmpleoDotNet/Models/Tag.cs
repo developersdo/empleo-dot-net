d7ea4b976ae934cc9691b85bc707521c4093a2c5
jeanrodriguez <jc_rc1812@hotmail.com>
2015-02-10 01:02:16 -0500

    agregando entidad nueva Tag. modificando entityBase. agregue relacion tag - jobOportunity (many to many)

---
+++ EmpleoDotNet/Models/Tag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet.Models
+{
+    public class Tag : EntityBase
+    {
+        #region Property
+        /// <summary>
+        /// Nombre del tag
+        /// </summary>
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [MaxLength(50,ErrorMessage = "Este campo no puede contener mas de 50 caracteres")]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+        #endregion
+
+        #region Navegation Properties
+        public List<JobOpportunity> Opportunities { get; set; } 
+
+        #endregion
+    }
+}
