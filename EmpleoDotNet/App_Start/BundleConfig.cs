387c5fb386531194c376ff2090aaac97f0115f44
Geronimo Romney <g.romney@gmail.com>
2015-04-22 17:35:25 -0400

    Cambio para ver la fecha de publicación como "Fuzzy Date"

--- EmpleoDotNet/App_Start/BundleConfig.cs
+++ EmpleoDotNet/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace EmpleoDotNet
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/moment.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
