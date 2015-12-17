af20d434be2d03a665fa997be742c7e76b234f1f
Luis Y. Ramirez <lyrc@outlook.com>
2015-12-17 00:37:22 -0400

    Added EnumerableExtension Method para convertir de una lista en un SelectList

---
+++ EmpleoDotNet/Helpers/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace EmpleoDotNet.Helpers
+{
+    public static class EnumerableExtensions
+    {
+        public static SelectList ToSelectList<T, TP1, TP2>(this IEnumerable<T> list, Expression<Func<T, TP1>> valueProperty, Expression<Func<T, TP2>> textProperty, object selectedValue = null) where T : class
+        {
+            var valueExpression = (MemberExpression)valueProperty.Body;
+            var dataValueField = valueExpression.Member.Name;
+
+            var textExpression = (MemberExpression)textProperty.Body;
+            var dataTextField = textExpression.Member.Name;
+
+            var result = new SelectList(list, dataValueField, dataTextField, selectedValue);
+
+            return result;
+        }
+    }
+}
