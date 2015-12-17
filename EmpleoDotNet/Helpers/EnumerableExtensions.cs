c65c60b8f1e6d0a53be60d0b5886258820e1f3a9
Luis Y. Ramirez <lyrc@outlook.com>
2015-12-17 16:22:57 -0400

    Added Xml docs for EnumerableExtensions class

--- EmpleoDotNet/Helpers/EnumerableExtensions.cs
+++ EmpleoDotNet/Helpers/EnumerableExtensions.cs
@@ -5,8 +5,19 @@ using System.Web.Mvc;
 
 namespace EmpleoDotNet.Helpers
 {
+    /// <summary>
+    /// Metodos extensores para IEnumerable
+    /// </summary>
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Metodo Extensor que permite convertir un IEnumerable en un SelectList usando Expressions 
+        /// </summary>
+        /// <param name="list">Lista que se convertira al SelectList</param>
+        /// <param name="valueProperty">Expression con la propiedad que contiene el Valor (key) en el SelectList</param>
+        /// <param name="textProperty">Expression con la propiedad que contiene lo que se desplegara cuando se seleccione un elemento en el SelectList</param>
+        /// <param name="selectedValue">Valor (key) que puede preseleccionarse a la hora de que se cargue el SelectList</param>
+        /// <returns>MVC SelectList</returns>
         public static SelectList ToSelectList<T, TP1, TP2>(this IEnumerable<T> list, Expression<Func<T, TP1>> valueProperty, Expression<Func<T, TP2>> textProperty, object selectedValue = null) where T : class
         {
             var valueExpression = (MemberExpression)valueProperty.Body;
