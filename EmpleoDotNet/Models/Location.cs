9a9f174acdd94c70e72ed059bf62eadca2296e34
Luis Y. Ramirez <lyrc@outlook.com>
2015-12-17 00:19:25 -0400

    - Creado Base super type para manejar los EntityTypeConfigurations - Removidos los DataAnnotations de JobOpportunity que manejaban las validaciones de MVC

--- EmpleoDotNet/Models/Location.cs
+++ EmpleoDotNet/Models/Location.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
-namespace EmpleoDotNet.Models
+﻿namespace EmpleoDotNet.Models
 {
-    public class Location:ISearchable
+    public class Location : ISearchable
     {
-        /// <summary>
-        /// Identificador único de cada ciudad
-        /// </summary>
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
     }
