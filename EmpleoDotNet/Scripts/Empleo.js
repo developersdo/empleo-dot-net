5d8ecc0c40bfde4939292f500bf0f2bd1d668475
RaulMonteroC <raulmonteroc01@gmail.com>
2016-01-04 20:11:40 -0400

    Refactored Empleo.js code file.

--- EmpleoDotNet/Scripts/Empleo.js
+++ EmpleoDotNet/Scripts/Empleo.js
@@ -1,38 +1,40 @@
-﻿function UpdateTotalCount() {    
-    var total = parseInt($('.total .value').html());
+﻿'use strict';
 
-    $('.total .value').html(total - 1);
-}
+var empleo = empleo || {};
+empleo = {
+    UpdateApprovalStatus: function (elem, jobId, isApproved) {
+        var url = "/Administration/UpdateApprovalStatus";
+        var data = {
+            jobId: jobId,
+            canShow: isApproved
+        };
 
-function UpdateApprovalStatus(jobId, isApproved) {
-    var self = $(this);
-    var url = "/Administration/UpdateApprovalStatus";
-    var data = {
-        jobId: jobId,
-        canShow: isApproved
-    };
-    
-    $.post(url, data, function () {
-        var parent = self.parent();
-        parent.empty();
-        parent.next().empty();
-        if (isApproved) 
-            parent.append('<span class="btn btn-primary">Aprobado</span>');
-        else
-            parent.append('<span class="btn btn-danger">Descartado</span>');
+        $.post(url, data, function () {
+            empleo.UpdateUI(elem.parent(), isApproved);
+        });
+    },
 
-        UpdateTotalCount();
-    });
-}
+    UpdateUI: function (elem, isApproved) {
+        elem.empty();
+        elem.next().empty();
+        if (isApproved) {
+            elem.append('<span class="btn btn-primary">Aprobado</span>');
+        } else {
+            elem.append('<span class="btn btn-danger">Descartado</span>');
+        }
 
-$(".approve-job").click(function () {
+        empleo.UpdateTotalCount();
+    },
+
+    UpdateTotalCount: function () {
+        var total = parseInt($('.total .value').html());
+        $('.total .value').html(total - 1);
+    }
+};
+
+$(".approve-job, .discard-job").click(function () {
     var self = $(this);
     var jobId = self.closest('tr').data('job');
-    UpdateApprovalStatus.call(self,jobId, true);
-});
-
-$(".discard-job").click(function () {
-    var self = $(this);
-    var jobId = self.closest('tr').data('job');
-    UpdateApprovalStatus.call(self,jobId, false);
+    var isApproved = self.hasClass('approve-job');
+    empleo.UpdateApprovalStatus(self, jobId, isApproved);
 });
