ed20877772636819a5ec83fd7675f8e8a0bdcbaf
Luis Ramirez <lyrc@outlook.com>
2016-01-07 17:22:25 -0400

    - Helper para mostrar de una manera mas agradable la descripcion del empleo - #118 Mostrar de marena mas agradable cuando halla un link en el detalle del empleo - #121 Removidas las referencias a archivos de fontawesome perdidos - #120 Removido el resize de los textarea's - Modificada la vista New de JobOpportunity para introducir de una manera mas agradable la descripcion del puesto - Cambios en el UI

---
+++ EmpleoDotNet/Helpers/JobOpportunityDescriptionBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmpleoDotNet.Helpers
+{
+    public static class JobOpportunityDescriptionBuilder
+    {
+        public static string BuildDescription(string rawDescription)
+        {
+            var result = new StringBuilder();
+
+            var splitedDescription = rawDescription.Split(new[] { Environment.NewLine },
+                StringSplitOptions.None);
+
+            foreach (var line in splitedDescription)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    result.AppendLine();
+                    continue;
+                }
+
+                var lineWithLink = Regex.Replace(line,
+                @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)",
+                "<a target='_blank' href='$1'>$1</a>");
+
+                if (line.Contains("http"))
+                    result.AppendLine(BuildHtmlTag("p", lineWithLink));
+                else
+                    result.AppendLine(BuildHtmlTag("p", line));
+            }
+
+            return result.ToString();
+        }
+        private static string BuildHtmlTag(string tag, string value)
+        {
+            return string.Format("<{0}>{1}</{0}>", tag, value);
+        }
+    }
+}
