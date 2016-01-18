fb22127842d3adb89e8133eecd06975a61ebe21a
Luis Ramirez <lyrc@outlook.com>
2016-01-18 18:01:06 -0400

    - Agregadas paginas de errores para los 5 errores mas comunes - Modificado el web.config para que redireccione segun el HttpErrorCode a cada pagina

---
+++ EmpleoDotNet/Controllers/ErrorController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+
+namespace EmpleoDotNet.Controllers
+{
+    public class ErrorController : Controller
+    {
+        /// <summary>
+        /// HTTP ERROR 400 (BAD REQUEST)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Http400()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// HTTP ERROR 401 (UNAUTHORIZED)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Http401()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// HTTP ERROR 403 (FORBIDDEN)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Http403()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// HTTP ERROR 404 (NOT FOUND)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Http404()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// HTTP ERROR 500 (INTERNAL SERVER ERROR)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Http500()
+        {
+            return View();
+        }
+    }
+}
