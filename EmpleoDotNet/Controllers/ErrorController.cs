55c14007169d43274be9aaabf9013d1e5310170d
Luis Ramirez <lyrc@outlook.com>
2016-01-25 11:40:55 -0400

    - Refactorizada la manera de devolver los errores - Se removieron todos los Views - Se agrego un View para manejar todos los tipos de error

--- EmpleoDotNet/Controllers/ErrorController.cs
+++ EmpleoDotNet/Controllers/ErrorController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web.Mvc;
+using EmpleoDotNet.ViewModel;
 
 namespace EmpleoDotNet.Controllers
 {
@@ -7,37 +9,61 @@ namespace EmpleoDotNet.Controllers
         /// <summary>
         /// HTTP ERROR 400 (BAD REQUEST)
         /// </summary>
-        /// <returns></returns>
         public ActionResult Http400()
         {
-            return View();
+            var viewModel = new ErrorViewModel
+            {
+                HttpStatusCode = HttpStatusCode.BadRequest,
+                Title = "Lo siento, no encontré lo que buscabas.",
+                SubTitle = "No te preocupes, estaremos arreglandolo en breve.",
+            };
+
+            return View("Index", viewModel);
         }
 
         /// <summary>
         /// HTTP ERROR 403 (FORBIDDEN)
         /// </summary>
-        /// <returns></returns>
         public ActionResult Http403()
         {
-            return View();
+            var viewModel = new ErrorViewModel
+            {
+                HttpStatusCode = HttpStatusCode.Forbidden,
+                Title = "Lo siento, no tienes permisos para ver esto.",
+                SubTitle = "Si sigues intentando tendré que tomar cartas en el asunto.",
+            };
+
+            return View("Index", viewModel);
         }
 
         /// <summary>
         /// HTTP ERROR 404 (NOT FOUND)
         /// </summary>
-        /// <returns></returns>
         public ActionResult Http404()
         {
-            return View();
+            var viewModel = new ErrorViewModel
+            {
+                HttpStatusCode = HttpStatusCode.NotFound,
+                Title = "Lo siento, no encontré lo que buscabas.",
+                SubTitle = "Descuida, No eres la única persona a quien esto le ha sucedido.",
+            };
+
+            return View("Index", viewModel);
         }
 
         /// <summary>
         /// HTTP ERROR 500 (INTERNAL SERVER ERROR)
         /// </summary>
-        /// <returns></returns>
         public ActionResult Http500()
         {
-            return View();
+            var viewModel = new ErrorViewModel
+            {
+                HttpStatusCode = HttpStatusCode.InternalServerError,
+                Title = "Oops! Ha ocurrido un error en nuestro sistema",
+                SubTitle = "Estaré revisandolo en breve y empleando mi fuerza para arreglarlo. <br/>Gracias por tu paciencia.",
+            };
+
+            return View("Index", viewModel);
         }
     }
 }
