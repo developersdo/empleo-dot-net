5fa22eee5ce416e859d9b393c3055cc7804bff80
Anyelo Almánzar Mercedes <anyeloamt@gmail.com>
2016-01-29 14:23:04 -0400

    Agregado formato más legible para las fechas en los empleos.

---
+++ EmpleoDotNet/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet.Helpers
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Returns the datetime in local format.
+        /// </summary>
+        /// <param name="dateTime">Date time</param>
+        /// <returns></returns>
+        public static string ToLocalFormat(this DateTime dateTime)
+        {
+            return dateTime.ToString("dd MMMM \\de yyyy", new CultureInfo("es-DO"));
+        }
+    }
+}
