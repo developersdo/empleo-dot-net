96a528ba21c56a3b0dede8bb34144a38dd9613bd
Enmanuel Toribio <eats007@gmail.com>
2016-02-04 21:06:56 -0400

    Added joel test to detail and added message to wizard

---
+++ EmpleoDotNet/Helpers/BoolExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet.Helpers
+{
+    public static class BoolExtensions
+    {
+        public static string ToYesNoString(this bool val)
+        {
+            return (val) ? "Si" : "No";
+        }
+    }
+}
