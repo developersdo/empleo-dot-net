441c6071d1e73e0525b7d7d1c486751f4e3bd3bb
Junior Gomez <junior.a.gomez@hotmail.com>
2016-02-11 07:37:24 -0400

    Friendly url cuando visualizas un empleo Issue #218 V2

---
+++ EmpleoDotNet/Helpers/UrlHelperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmpleoDotNet.Helpers
+{
+    public static class UrlHelperExtensions
+    {
+        /// <summary>
+        /// Sanitiza un URL
+        /// </summary>
+        /// <remarks>http://stackoverflow.com/questions/6716832/sanitizing-string-to-url-safe-format</remarks>
+        public static string SanitizeUrl(this string strToSanitize)
+        {
+            return strToSanitize == null
+                ? null
+                : Regex.Replace(strToSanitize, @"[^A-Za-z0-9_~]+", "-");
+        }
+
+        public static string SeoUrl(int id, string title)
+        {
+            return string.IsNullOrEmpty(title) ? id.ToString() : $"{id}-{SanitizeUrl(title)}";
+        }
+    }
+}
