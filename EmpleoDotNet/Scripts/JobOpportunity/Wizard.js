0e7ac15f0cd10f137a6fbc76779ce36ab5c1023c
RaulMonteroC <raulmonteroc01@gmail.com>
2016-02-11 23:20:22 -0400

    Refactored Wizard.js file to make it more readable.

--- EmpleoDotNet/Scripts/JobOpportunity/Wizard.js
+++ EmpleoDotNet/Scripts/JobOpportunity/Wizard.js
@@ -1,5 +1,8 @@
-﻿var currentSection = 0;
-$(document).ready(function () {
+﻿"use strict";
+
+var Wizard = function () {
+    var currentSection = 0;
+    var sections = ["INFORMACIÓN DE LA EMPRESA", "INFORMACIÓN DEL PUESTO", "LA PRUEBA DE JOEL"];
     var owl = $("#wizard").owlCarousel({
         slideSpeed: 300,
         paginationSpeed: 400,
@@ -11,11 +14,7 @@ $(document).ready(function () {
         dots: false,
         touchDrag: false,
         mouseDrag: false
-    });    
-    var sections = ["INFORMACIÓN DE LA EMPRESA", "INFORMACIÓN DEL PUESTO", "LA PRUEBA DE JOEL"];
-    $('.section-title').html(sections[currentSection]);
-    $('.tab').removeClass('active');
-    $('.section-1').addClass('active');
+    });
 
     var validateScreen = function (callback) {
         var parsleyGroup = $(".active .item").data('parsley-group');
@@ -24,37 +23,57 @@ $(document).ready(function () {
         }
     };
 
-    $('form').submit(function (e) {
-        if(!$('form').parsley().validate()) {
-            e.preventDefault();
-        }
-    });
-    $(window).keydown(function(e) {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-        }
-    });
-    $("input").keydown(function(e) {
-        if (e.keyCode === 13) {
-            validateScreen(function() {
-                owl.trigger('next');
-            });
-        }
-    });
-    $(".btn-next").click(function () {
-        validateScreen(function () {
-            owl.trigger('next');
-            currentSection++;
-            $('.section-title').html(sections[currentSection]);
-            $('.tab').removeClass('active');
-            $('.section-' + (currentSection+1)).addClass('active');
-        });
-    });
-    $(".btn-prev").click(function () {
-        owl.trigger('prev');
-        currentSection--;
+    var changeActiveTab = function () {
         $('.section-title').html(sections[currentSection]);
         $('.tab').removeClass('active');
-        $('.section-' + (currentSection+1)).addClass('active');
-    });
+        $('.section-' + (currentSection + 1)).addClass('active');
+    }
+
+    $('.section-title').html(sections[currentSection]);
+    $('.tab').removeClass('active');
+    $('.section-1').addClass('active');
+
+    return {
+        next: function () {
+            validateScreen(function () {
+                owl.trigger('next');
+                currentSection++;
+                changeActiveTab();
+            });
+        },
+
+        previous: function () {
+            owl.trigger('prev');
+            currentSection--;
+            changeActiveTab();
+        },
+
+        submitForm: function() {
+            if (!$('form').parsley().validate()) {
+                e.preventDefault();
+            }
+        },
+
+        preventEnter: function () {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+            }
+        },
+        goNextOnEnter: function () {
+            if (e.keyCode === 13) {
+                validateScreen(function () {
+                    owl.trigger('next');
+                });
+            }
+        }
+    };
+}
+
+$(function () {
+    var wizard = new Wizard();
+    $('form').submit(function (e) { wizard.submitForm(); });
+    $(window).keydown(function (e) { wizard.preventEnter(); });
+    $("input").keydown(function (e) { wizard.goNextOnEnter(); });
+    $(".btn-next").click(function () { wizard.next();});
+    $(".btn-prev").click(function () { wizard.previous(); });
 });
