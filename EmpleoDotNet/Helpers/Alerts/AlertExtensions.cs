2fcb66066dc2cba09db55dfdef1c737535ebd8d9
Enmanuel Toribio <eats007@gmail.com>
2016-02-16 11:30:53 -0400

    Refactoring the way alerts are shown

---
+++ EmpleoDotNet/Helpers/Alerts/AlertExtensions.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace EmpleoDotNet.Helpers.Alerts
+{
+	public static class AlertExtensions
+	{
+		const string Alerts = "_Alerts";
+
+		public static List<Alert> GetAlerts(this TempDataDictionary tempData)
+		{
+			if (!tempData.ContainsKey(Alerts))
+			{
+				tempData[Alerts] = new List<Alert>();
+			}
+
+			return (List<Alert>) tempData[Alerts];
+		}
+
+		public static ActionResult WithSuccess(this ActionResult result, string message)
+		{
+			return new AlertDecoratorResult(result, "alert-success", message);
+		}
+
+		public static ActionResult WithInfo(this ActionResult result, string message)
+		{
+			return new AlertDecoratorResult(result, "alert-info", message);
+		}
+
+		public static ActionResult WithWarning(this ActionResult result, string message)
+		{
+			return new AlertDecoratorResult(result, "alert-warning", message);
+		}
+
+		public static ActionResult WithError(this ActionResult result, string message)
+		{
+			return new AlertDecoratorResult(result, "alert-danger", message);
+		}
+	}
+}
