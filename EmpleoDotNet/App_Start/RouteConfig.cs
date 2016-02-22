d0a0ef2dd324181a4cc01582a011b48f45113f74
RaulMonteroC <raulmonteroc01@gmail.com>
2016-02-21 21:05:53 -0400

    Added job route custom configuration.

--- EmpleoDotNet/App_Start/RouteConfig.cs
+++ EmpleoDotNet/App_Start/RouteConfig.cs
@@ -10,6 +10,25 @@ namespace EmpleoDotNet
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "All jobs",
+                url: "jobs",
+                defaults: new { controller = "JobOpportunity", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "job",
+                url: "job/{id}",
+                defaults: new { controller = "JobOpportunity", action = "Detail" },
+                constraints: new {id = @"\d+[a-z-A-Z-]+" }
+            );
+
+            routes.MapRoute(
+                name: "Default job route",
+                url: "job/{action}",
+                defaults: new { controller = "JobOpportunity", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
