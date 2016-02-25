b837c31ebfaed91e097d7c8069ea9427e8accc95
Luis Ramirez <lyrc@outlook.com>
2016-02-24 20:03:24 -0400

    Added Under Maintenance Feature
    - Added Global Action Filter
    - Added view under maintenance

---
+++ EmpleoDotNet/Helpers/UnderMaintenanceFilterAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EmpleoDotNet.Helpers
+{
+    public class UnderMaintenanceFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.ActionDescriptor.ControllerDescriptor.ControllerName.Contains("Error") ||
+                filterContext.ActionDescriptor.ControllerDescriptor.ControllerName.Contains("Credits") ||
+                filterContext.ActionDescriptor.ControllerDescriptor.ControllerName.Contains("UnderMaintenance")
+                )
+            {
+                base.OnActionExecuting(filterContext);
+                return;
+            }
+
+            bool underMaintenance;
+
+            bool.TryParse(ConfigurationManager.AppSettings["UnderMaintenance"], out underMaintenance);
+
+            if (underMaintenance)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary
+                    {
+                        { "controller", "UnderMaintenance"},
+                        { "action", "Index"}
+                    });
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
