a6dcb247f6e470032e7fd6d6ce12201cd42d73c6
Raul Montero <raulmonteroc01@gmail.com>
2016-04-12 02:01:32 +0000

    Now the wizard form can edit a job opportunity if the id exists in the database.

--- EmpleoDotNet.AppServices/JobOpportunityService.cs
+++ EmpleoDotNet.AppServices/JobOpportunityService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using EmpleoDotNet.Core.Domain;
 using EmpleoDotNet.Core.Dto;
@@ -8,12 +9,43 @@ using PagedList;
 namespace EmpleoDotNet.AppServices
 {
     public class JobOpportunityService : IJobOpportunityService
-    {
+    {        
         public void CreateNewJobOpportunity(JobOpportunity jobOpportunity, string userid)
         {
             jobOpportunity.UserProfile = _userProfileRepository.GetByUserId(userid);
             _jobOpportunityRepository.Add(jobOpportunity);
             _jobOpportunityRepository.SaveChanges();
+        }
+
+        public void UpdateJobOpportunity(int id, JobOpportunity updatedJob)
+        {
+            var existingJob = _jobOpportunityRepository.GetJobOpportunityById(id);
+
+            existingJob.Title = updatedJob.Title;
+            existingJob.Approved = updatedJob.Approved;
+            existingJob.Category = updatedJob.Category;
+            existingJob.CompanyEmail = updatedJob.CompanyEmail;
+            existingJob.CompanyLogoUrl = updatedJob.CompanyLogoUrl;
+            existingJob.CompanyName = updatedJob.CompanyName;
+            existingJob.CompanyUrl = updatedJob.CompanyUrl;
+            existingJob.Description = updatedJob.Description;
+            existingJob.HowToApply = updatedJob.HowToApply;
+            existingJob.IsActive = updatedJob.IsActive;
+            existingJob.IsRemote = updatedJob.IsRemote;
+
+            existingJob.JobOpportunityLikes = updatedJob.JobOpportunityLikes;
+            existingJob.JobOpportunityLocation = updatedJob.JobOpportunityLocation;
+            existingJob.JobOpportunityLocationId = updatedJob.JobOpportunityLocationId;
+
+            existingJob.JobType = updatedJob.JobType;
+            existingJob.JoelTest = updatedJob.JoelTest;
+            existingJob.JoelTestId = updatedJob.JoelTestId;
+            existingJob.Location = updatedJob.Location;
+            existingJob.LocationId = updatedJob.LocationId;
+            existingJob.Tags = updatedJob.Tags;
+
+            _jobOpportunityRepository.SaveChanges();
+
         }
 
         public List<JobOpportunity> GetCompanyRelatedJobs(int id, string name)
@@ -48,6 +80,10 @@ namespace EmpleoDotNet.AppServices
             return _jobOpportunityRepository.GetMainJobCategoriesCount();
         }
 
+        public bool JobExists(int id)
+        {
+            return _jobOpportunityRepository.JobExists(id);
+        }
 
         public JobOpportunityService(
             IJobOpportunityRepository jobOpportunityRepository,
