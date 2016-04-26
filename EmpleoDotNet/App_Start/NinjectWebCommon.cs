10733d128b5bf22951a17417dd8036903e5ef951
Eliezer Pujols <eliezerpujols@outlook.com>
2016-04-26 18:55:43 -0400

    Fixed Footer

--- EmpleoDotNet/App_Start/NinjectWebCommon.cs
+++ EmpleoDotNet/App_Start/NinjectWebCommon.cs
@@ -11,20 +11,20 @@ namespace EmpleoDotNet.App_Start
     using Ninject;
     using Ninject.Web.Common;
 
-    public static class NinjectWebCommon
+    public static class NinjectWebCommon 
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start()
+        public static void Start() 
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-
+        
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -32,7 +32,7 @@ namespace EmpleoDotNet.App_Start
         {
             bootstrapper.ShutDown();
         }
-
+        
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -61,7 +61,6 @@ namespace EmpleoDotNet.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Load<EmpleadoModule>();
-        }
+        }        
     }
 }
