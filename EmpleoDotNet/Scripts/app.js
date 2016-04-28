e50b73e1262e95634ffe4d9f96c679856fe8e21a
Raul Montero <raulmonteroc01@gmail.com>
2016-04-28 03:53:17 +0000

    Added method to clean empty get parameters when posted from a form.

--- EmpleoDotNet/Scripts/app.js
+++ EmpleoDotNet/Scripts/app.js
@@ -5,4 +5,19 @@
             window.location = detailUrl;
         }
     };
+
+    $('form').submit(function () {
+        var self = $(this);
+        var parameters = self.serialize().match(/(\w+=)/g);
+
+        for (var i = 0; i < parameters.length; i++) {
+            var param = parameters[i];
+            param = param.slice(0,param.length - 1);
+            var formElem = self.find('*[name=' + param + ']');
+
+            if (!formElem.val()) {
+                formElem.prop('disabled', true);
+            }
+        }
+    });
 })()
