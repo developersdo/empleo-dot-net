a7c5950457b157040d3707cebe06cf9f1b1c9acf
Willam Bello <willjobs94@gmail.com>
2016-04-29 10:52:26 -0400

    Create RSS feed for job opportunities

---
+++ EmpleoDotNet/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpleoDotNet
+{
+    public static class Constants
+    {
+        public static string RssTitle => "Emplea.do";
+        public static string RssDescription => "El mejor lugar para conseguir el empleo de tus sueños";
+    }
+}
