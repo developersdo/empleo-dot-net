7a0b262726b14d076c7ad2147fa9f186a3bd4432
Raul Montero <raulmonteroc01@gmail.com>
2016-05-01 11:24:17 -0400

    Friendly search URL (#469)
    * Added method to clean empty get parameters when posted from a form.
    
    * Simplified URLCleaner method and added some missing validations.

--- EmpleoDotNet/Scripts/app.js
+++ EmpleoDotNet/Scripts/app.js
@@ -5,4 +5,18 @@
             window.location = detailUrl;
         }
     };
+
+    $('form[method="get"]').submit(function () {
+        var self = $(this);
+        var parameters = self.serialize().match(/(\w+=)/g);
+        
+        for (var i = 0; i < parameters.length; i++) {
+            var param = parameters[i].slice(0,-1);
+            var formElem = self.find('*[name=' + param + ']');
+
+            if (!formElem.first().val() || !formElem.first().prop('checked') ) {
+                formElem.prop('disabled', true);
+            }
+        }
+    });
 })()
