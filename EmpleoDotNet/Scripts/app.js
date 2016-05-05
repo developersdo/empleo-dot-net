2b5a250bbc024932a9c0fc63208f9dc408a55ad2
Willam Bello <willjobs94@gmail.com>
2016-05-04 23:44:41 -0400

    Fix search not working on CI

--- EmpleoDotNet/Scripts/app.js
+++ EmpleoDotNet/Scripts/app.js
@@ -9,16 +9,15 @@
 
     $('form[method="get"]').submit(function () {
         var self = $(this);
-        var parameters = self.serialize().match(/(\w+=)/g);
-
-        for (var i = 0; i < parameters.length; i++) {
-            var param = parameters[i].slice(0, -1);
-            var formElem = self.find("*[name=" + param + "]");
-
-            if (!formElem.first().val() || !formElem.first().prop("checked")) {
+        var parameters = self.serialize();
+        parameters.split("&").forEach(function (val) {
+            var param = val.replace(/=\w+|=/, "");
+            var paramValue = val.replace(/\w+=/, "");
+            if (paramValue === "") {
+                var formElem = self.find("*[name=" + param + "]");
                 formElem.prop("disabled", true);
             }
-        }
+        });
     });
 
     $("#confirm-delete").on("show.bs.modal", function (e) {
@@ -27,48 +26,8 @@
             .html("<h4>" + $(e.relatedTarget)
             .data("title") + "</h4>");
         $(this)
-            .find(".btn-ok")
+           .find(".btn-ok")
             .attr("href", $(e.relatedTarget)
             .data("href"));
     });
-
-    $(".switch")
-        .click(function () { 
-            toggleHidePost($(this), toggleSwitchInput);
-        });
-
-    $("#switch-btn")
-        .click(function () {
-            toggleHidePost($(this), toggleSwitchBtn);
-        });
-
-    function toggleHidePost($node, callback) {
-        var url = $node.data("href");
-        var title = $node.data("title");
-        $.post(url, "title=" + title, function (data) {
-            callback(data, $node);
-        });
-    }
-
-    function toggleSwitchBtn(data, $node) {
-        if (data.isHidden) {
-            $node.html("Mostrar");
-            $node.toggleClass("btn-toggle-hide");
-            $node.toggleClass("btn-toggle-show");
-        } else {
-            $node.html("Ocultar");
-            $node.toggleClass("btn-toggle-hide");
-            $node.toggleClass("btn-toggle-show");
-        }
-    }
-
-    function toggleSwitchInput(data, $node) {
-        var $input = $node.find(":input");
-        if (data.isHidden) {
-            $input.prop("checked", data.isHidden);
-        } else {
-            $input.prop("checked", false);
-        }
-    }
-
 })()
