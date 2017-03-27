92d9804227ca89e9bc4c65374aaf0935fb85f9fd
jpozo20 <jpozo@outlook.com>
2017-03-27 19:01:45 -0400

    Add empty migration so EF doesn't complain
    After fixing a problem with the branch, EF was complaining
    when applying the latest migration so I added an
    empty migration to apply the pending changes

---
+++ EmpleoDotNet.Data/Migrations/201703272259044_EmptyMigrationSoEFDoesntCompain.cs
@@ -0,0 +1,18 @@
+namespace EmpleoDotNet.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyMigrationSoEFDoesntCompain : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
