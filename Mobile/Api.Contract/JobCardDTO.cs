c88ed855cef05b00678d0c12830afae87a58ab42
Claudio Sanchez <claudio@megsoftconsulting.com>
2019-11-07 20:13:22 -0400

    Made some changes to the API to include all of the data needed to List Jobs. (#565)

--- Mobile/Api.Contract/JobCardDTO.cs
+++ Mobile/Api.Contract/JobCardDTO.cs
@@ -5,17 +5,18 @@ namespace Api.Contract
 	public class JobCardDTO
 	{
 		public string Link{ get; set; }
-
-		public string Job{ get; set; }
-
-		public string Employee{ get; set; }
-
+        public string CompanyName { get; set; }
+        public string Title { get; set; }
 		public string JobType{ get; set; }
-
 		public string Location{ get; set; }
-
-		public bool IsRemote{ get; set; }
-	}
+        public DateTime PublishedDate { get; set; }
+        public bool IsRemote{ get; set; }
+        public int ViewCount { get; set; }
+        public int Likes { get; set; }
+        public string CompanyLogoUrl { get; set; }
+        public string Description { get; set; }
+        public string HowToApply { get; set; }
+    }
 
 }
 
